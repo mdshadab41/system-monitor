f700d0ba25071c5ede9a4ab531ed84bbf330adb0
mdshadab41 <shadabjhu00447@gmail.com>
2026-05-31 09:28:15 +0000

    added S3 backup upload script

---
+++ scripts/backup_s3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Load config
+source /home/cloudshell-user/system-monitor/config/config.cfg
+
+# Timestamp
+TIMESTAMP=$(date '+%Y-%m-%d_%H-%M-%S')
+
+# S3 bucket name
+S3_BUCKET="shadab-system-monitor-backups"
+
+# Start message
+echo "[$TIMESTAMP] Uploading backups to S3..."
+echo "[$TIMESTAMP] Uploading backups to S3..." >> $LOG_DIR/backup.log
+
+# Upload to S3 excluding .git files
+aws s3 sync $BACKUP_DIR s3://$S3_BUCKET/backups/ \
+    --exclude "*.git*" \
+    --exclude "*/.git/*"
+
+# Success message
+echo "[$TIMESTAMP] S3 upload complete!"
+echo "[$TIMESTAMP] S3 upload complete!" >> $LOG_DIR/backup.log
